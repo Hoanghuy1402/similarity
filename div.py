1c613ca9bfc89c519fa0eb06c622397bb0a228bb
Hoanghuy1402 <121704215+Hoanghuy1402@users.noreply.github.com>
2024-11-14 23:41:04 +0700

    Update div.py

--- div.py
+++ div.py
@@ -349,7 +349,7 @@ def Tabu_search_for_CVRP(CC):
     list_init = []
     
     start_time = time.time()
-    current_sol5 = Function.initial_solution3()
+    current_sol5 = Function.initial_solution7()
     list_init.append(current_sol5)
 
     
