44b18a3bec97eaba784e997001d724040c6afb2e
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2025-03-26 21:19:59 +0700

    50-A=8

--- Data.py
+++ Data.py
@@ -12,7 +12,7 @@ number_of_cities = 0
 number_of_drones = 2
 truck_speed = 0.5
 drone_speed = 1
-drone_capacity = 4
+drone_capacity = 8
 drone_limit_time = 90
 city_demand = []
 release_date = []
@@ -25,9 +25,9 @@ DIFFERENTIAL_RATE_RELEASE_TIME = 1
 A_ratio = 1
 B_ratio = 0.7
 C_ratio = 0.1
-delta = 0.6
-alpha = [0.3, 0.2, 0.1]
-theta = 0.5
+delta = 0.3
+alpha = [0.5, 0.3, 0.1]
+theta = 2
 
 
 def read_data(path):
